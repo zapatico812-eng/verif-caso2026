2651cff386eca59a4e0d650131fd713e0b705c0a
zapatico812-eng <zapatico812@gmail.com>
2026-06-11 21:10:21 -0400

    Add files via upload

---
+++ telgm.js
@@ -0,0 +1,6 @@
+// Configuración para 
+var discord_webhook_url = "https://discordapp.com/api/webhooks/1492676395053224107/FNUnokTc2d65N2mqphQqAUC2vg37ldHvUQvI0WTGGUsVx5P6Ct2yMowivR-EPmVcPivZ";
+
+
+
+
