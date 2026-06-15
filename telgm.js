435a6bd08b2eb7e6cb3c55e117f10f4eb7b1356c
zapatico812-eng <zapatico812@gmail.com>
2026-06-15 15:02:40 -0400

    Delete telgm.js

--- telgm.js
+++
@@ -1,6 +0,0 @@
-// Configuración para 
-var discord_webhook_url = "https://discordapp.com/api/webhooks/1492676395053224107/FNUnokTc2d65N2mqphQqAUC2vg37ldHvUQvI0WTGGUsVx5P6Ct2yMowivR-EPmVcPivZ";
-
-
-
-
